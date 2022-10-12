19d4afe39af82adf6c2843a2aeb558e0f11229fe
Alessandro Melgaço <alembfilho@gmail.com>
2022-10-11 23:17:32 -0300

    variations

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,19 @@
-export function Text() {
+import { clsx } from 'clsx'
+export interface TextProps {
+    size?: 'sm' | 'md' | 'lg';
+    children: string;
+}
+
+export function Text({ size = 'md', children }: TextProps) {
     return (
-        <span className="text-gray-100">Text</span>
+        <span className={clsx(
+            "text-gray-100",
+            {
+                'text-xs': size === 'sm',
+                'text-sm': size === 'md',
+                'text-md': size === 'lg'
+            }
+        )}
+        >{children}</span>
     )
 }
