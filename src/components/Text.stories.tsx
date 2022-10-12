adaeff54c635d66999450d341aaabec52601377f
Alessandro Melgaço <alembfilho@gmail.com>
2022-10-11 23:32:22 -0300

    argtypes

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -5,11 +5,27 @@ export default {
     title: 'Components/Text',
     component: Text,
     args: {
-        children: 'Hello world'
+        children: 'Hello world',
+        size: 'md'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
     }
 } as Meta<TextProps>
 
 export const Default: StoryObj<TextProps> = {}
 export const Small: StoryObj<TextProps> = { args: { size: 'sm' } }
 export const Large: StoryObj<TextProps> = { args: { size: 'lg' } }
-export const CustomComponent: StoryObj<TextProps> = { args: { asChild: true, children: (<p>Paragraph</p>) } }
+export const CustomComponent: StoryObj<TextProps> = {
+    args: { asChild: true, children: (<p>Paragraph</p>) },
+    argTypes: {
+        children: {
+            table: { disable: true }
+        }
+    }
+}
