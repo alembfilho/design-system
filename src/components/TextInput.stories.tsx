71960d3d0d5e8dee4905772fa7bc7ea7d6ce4b23
Alessandro Melgaço <alembfilho@gmail.com>
2022-10-13 14:10:05 -0300

    input

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,11 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { TextInput, TextInputProps } from "./TextInput"
+import { Envelope } from 'phosphor-react'
 
 export default {
     title: 'Components/TextInput',
     component: TextInput,
     args: {
         placeholder: 'e-mail',
+        icon: <Envelope />
     },
 } as Meta<TextInputProps>
 
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,13 +1,18 @@
 import { clsx } from 'clsx'
 import { InputHTMLAttributes, ReactNode } from 'react';
 import { Envelope } from 'phosphor-react'
+import { Slot } from '@radix-ui/react-slot';
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    icon?: ReactNode
+}
 
 export function TextInput(props: TextInputProps) {
+    // const Icon = props.icon ? 'div' : Slot
     return (
-        <div className='flex gap-3 items-center py-4 px-3 rounded w-full bg-gray-800 focus-within:ring-2 ring-cyan-300'>
-            <Envelope className='text-gray-400 w-6 h-6' />
+        <div className='flex gap-3 items-center py-4 px-3 h-12 rounded w-full bg-gray-800 focus-within:ring-2 ring-cyan-300'>
+
+            <Slot className='text-gray-400 w-6 h-6'>{props.icon}</Slot>
             <input className={clsx(
                 `bg-transparent text-xs text-gray-100 
                 placeholder:text-gray-400
